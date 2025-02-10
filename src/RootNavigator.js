3d7fa0c14fad0d14fc5a75c9ee0c094681baf7de
ansumanp_kloudgin <ansumanp@kloudgin.com>
2025-02-10 21:51:18 +0530

    Screen change according to user token storage logic

--- src/RootNavigator.js
+++ src/RootNavigator.js
@@ -3,15 +3,31 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./Screens/HomeScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import RegisterScreen from "./Screens/RegisterScreen";
+import MainAppScreen from "./Screens/MainAppScreen";
+import { useSelector } from "react-redux";
 
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
+  const { user } = useSelector((state) => state.auth);
+  
+  let shownScreen = null;
+
+  if (user) {
+    shownScreen = <Stack.Screen name='MainApp' component={MainAppScreen} />;
+  } else {
+    shownScreen = (
+      <>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+      </>
+    );
+  }
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='Home' component={HomeScreen} />
-      <Stack.Screen name='Login' component={LoginScreen} />
-      <Stack.Screen name='Register' component={RegisterScreen} />
+      {shownScreen}
     </Stack.Navigator>
   );
 }
---
+++ src/Screens/MainAppScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function MainAppScreen() {
+  return (
+	<View>
+	  <Text>MainAppScreen</Text>
+	</View>
+  )
+}
+
+const styles = StyleSheet.create({})
