6b8f35dfe6e9ff52563127648998356d99573fdf
aspx003 <ansupatra003@gmail.com>
2025-05-31 09:31:21 +0530

    FIXED: Button gets hidden when entering formdata in Register and Login Screen

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,123 +1,102 @@
-import React, { useContext, useState } from "react";
-import {
-  ActivityIndicator,
-  Alert,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
-import { ms, mvs, s, vs } from "react-native-size-matters";
+import React,{ useContext,useState } from "react";
+import { ActivityIndicator,Alert,KeyboardAvoidingView,StyleSheet,Text,TextInput,View,ScrollView } from "react-native";
+import { ms,mvs,s,vs } from "react-native-size-matters";
 import Button from "../Components/Button";
 import { AuthContext } from "../Context/auth-context";
-import { loginUser, fetchUserProfile } from "../Utils/general/authUtility";
+import { loginUser,fetchUserProfile } from "../Utils/general/authUtility";
 import { colors } from "../constants/colors";
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isAuthenticating, setIsAuthenticating] = useState(false);
+	const [email,setEmail] = useState("");
+	const [password,setPassword] = useState("");
+	const [isAuthenticating,setIsAuthenticating] = useState(false);
 
-  const authContext = useContext(AuthContext);
+	const authContext = useContext(AuthContext);
 
-  async function signInHandler() {
-    if (!email && !password) {
-      return Alert.alert("Please fill all fields!", "Try Again!");
-    }
+	async function signInHandler() {
+		if (!email && !password) {
+			return Alert.alert("Please fill all fields!","Try Again!");
+		}
 
-    setIsAuthenticating(true);
+		setIsAuthenticating(true);
 
-    try {
-      const token = await loginUser(email, password);
-      const username = await fetchUserProfile(token);
-      authContext.authenticate(token, username);
-    } catch (error) {
-      Alert.alert("Login Failed!", "Pls try again or contact your dev!");
-    }
+		try {
+			const token = await loginUser(email,password);
+			const username = await fetchUserProfile(token);
+			authContext.authenticate(token,username);
+		} catch (error) {
+			Alert.alert("Login Failed!","Pls try again or contact your dev!");
+		}
 
-    setIsAuthenticating(false);
-  }
+		setIsAuthenticating(false);
+	}
 
-  if (isAuthenticating) {
-    return (
-      <View style={styles.activityIndicator}>
-		<Text>
-        	<ActivityIndicator size='large' color={colors.buttonColor} />;
-		</Text>
-      </View>
-    );
-  }
+	if (isAuthenticating) {
+		return (
+			<View style={styles.activityIndicator}>
+				<Text>
+					<ActivityIndicator size='large' color={colors.buttonColor} />;
+				</Text>
+			</View>
+		);
+	}
 
-  return (
-    <View style={styles.mainContainer}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerContainerText}>Login</Text>
-      </View>
-      <View>
-        <Text style={styles.captionText}>Good to have you back!</Text>
-      </View>
-      <View style={styles.formContainer}>
-        <TextInput
-          style={styles.textInput}
-          keyboardType='email-address'
-          placeholder='Email'
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          placeholderTextColor={colors.placeholderTextColor}
-        />
-        <TextInput
-          style={styles.textInput}
-          secureTextEntry={true}
-          placeholder='Password'
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          placeholderTextColor={colors.placeholderTextColor}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button buttonName='Continue' onPress={signInHandler} />
-      </View>
-    </View>
-  );
+	return (
+		<KeyboardAvoidingView style={styles.mainContainer} behavior="padding">
+			<View style={styles.headerContainer}>
+				<Text style={styles.headerContainerText}>Login</Text>
+			</View>
+			<View>
+				<Text style={styles.captionText}>Good to have you back!</Text>
+			</View>
+			<View style={styles.formContainer}>
+				<TextInput style={styles.textInput} keyboardType='email-address' placeholder='Email' value={email} onChangeText={(text) => setEmail(text)} placeholderTextColor={colors.placeholderTextColor} />
+				<TextInput style={styles.textInput} secureTextEntry={true} placeholder='Password' value={password} onChangeText={(text) => setPassword(text)} placeholderTextColor={colors.placeholderTextColor} />
+			</View>
+			<View style={styles.buttonContainer}>
+				<Button buttonName='Continue' onPress={signInHandler} />
+			</View>
+		</KeyboardAvoidingView>
+	);
 }
 
 const styles = StyleSheet.create({
-  activityIndicator: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  mainContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  headerContainer: {
-    marginBottom: mvs(25),
-  },
-  headerContainerText: {
-    fontSize: mvs(40),
-    color: colors.textColor,
-	fontFamily: 'Montserrat'
-  },
-  captionText: {
-    fontSize: ms(20),
-    color: colors.textColor,
-	fontFamily: 'Lato'
-  },
-  formContainer: {
-    marginVertical: vs(25),
-  },
-  textInput: {
-    paddingVertical: vs(10),
-    paddingHorizontal: s(5),
-    borderBottomWidth: s(1),
-    width: s(300),
-    marginVertical: vs(5),
-    borderColor: colors.textColor,
-    color: colors.textColor,
-  },
-  buttonContainer: {
-    width: s(200),
-  },
+	activityIndicator: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	mainContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	headerContainer: {
+		marginBottom: mvs(25),
+	},
+	headerContainerText: {
+		fontSize: mvs(40),
+		color: colors.textColor,
+		fontFamily: "Montserrat",
+	},
+	captionText: {
+		fontSize: ms(20),
+		color: colors.textColor,
+		fontFamily: "Lato",
+	},
+	formContainer: {
+		marginVertical: vs(25),
+	},
+	textInput: {
+		paddingVertical: vs(10),
+		paddingHorizontal: s(5),
+		borderBottomWidth: s(1),
+		width: s(300),
+		marginVertical: vs(5),
+		borderColor: colors.textColor,
+		color: colors.textColor,
+	},
+	buttonContainer: {
+		width: s(200),
+	},
 });
--- src/Screens/RegisterScreen.js
+++ src/Screens/RegisterScreen.js
@@ -1,118 +1,119 @@
-import React, { useState } from "react";
+import React,{ useState } from "react";
 import {
-  ActivityIndicator,
-  Alert,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
+	ActivityIndicator,
+	Alert,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+	KeyboardAvoidingView
 } from "react-native";
-import { ms, mvs, s, vs } from "react-native-size-matters";
+import { ms,mvs,s,vs } from "react-native-size-matters";
 import Button from "../Components/Button";
 import { registerUser } from "../Utils/general/authUtility";
 import { colors } from "../constants/colors";
 
 export default function RegisterScreen({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [isAuthenticating, setIsAuthenticating] = useState(false);
+	const [email,setEmail] = useState("");
+	const [username,setUsername] = useState("");
+	const [password,setPassword] = useState("");
+	const [isAuthenticating,setIsAuthenticating] = useState(false);
 
-  async function registerHandler() {
+	async function registerHandler() {
 
-	if(!email && !username && !password) {
-		return Alert.alert("Please fill all fields!", "Try Again!");
+		if (!email && !username && !password) {
+			return Alert.alert("Please fill all fields!","Try Again!");
+		}
+
+		setIsAuthenticating(true);
+		try {
+			await registerUser(username,email,password);
+		} catch (error) {
+			Alert.alert("Register Failed!","Pls try again or contact your dev!");
+		}
+		setIsAuthenticating(false);
+		navigation.replace("Login");
 	}
 
-    setIsAuthenticating(true);
-    try {
-      await registerUser(username, email, password);
-    } catch (error) {
-      Alert.alert("Register Failed!", "Pls try again or contact your dev!");
-    }
-    setIsAuthenticating(false);
-    navigation.replace("Login");
-  }
+	if (isAuthenticating) {
+		return <ActivityIndicator size='large' />;
+	}
 
-  if (isAuthenticating) {
-    return <ActivityIndicator size='large' />;
-  }
-
-  return (
-    <View style={styles.mainContainer}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerContainerText}>Get Started</Text>
-      </View>
-      <View>
-        <Text style={styles.captionText}>
-          We’ll need your name and email address to get started.
-        </Text>
-      </View>
-      <View style={styles.formContainer}>
-        <TextInput
-          style={styles.textInput}
-          keyboardType='email-address'
-          placeholder='Email'
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-		  placeholderTextColor={colors.placeholderTextColor}
-        />
-        <TextInput
-          style={styles.textInput}
-          placeholder='Name'
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-		  placeholderTextColor={colors.placeholderTextColor}
-        />
-        <TextInput
-          style={styles.textInput}
-          secureTextEntry={true}
-          placeholder='Password'
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-		  placeholderTextColor={colors.placeholderTextColor}
-        />
-      </View>
-	  <View style={styles.buttonContainer}>
-      	<Button buttonName='Login' onPress={registerHandler} />
-	  </View>
-    </View>
-  );
+	return (
+		<KeyboardAvoidingView style={styles.mainContainer} behavior="padding">
+			<View style={styles.headerContainer}>
+				<Text style={styles.headerContainerText}>Get Started</Text>
+			</View>
+			<View>
+				<Text style={styles.captionText}>
+					We’ll need your name and email address to get started.
+				</Text>
+			</View>
+			<View style={styles.formContainer}>
+				<TextInput
+					style={styles.textInput}
+					keyboardType='email-address'
+					placeholder='Email'
+					value={email}
+					onChangeText={(text) => setEmail(text)}
+					placeholderTextColor={colors.placeholderTextColor}
+				/>
+				<TextInput
+					style={styles.textInput}
+					placeholder='Name'
+					value={username}
+					onChangeText={(text) => setUsername(text)}
+					placeholderTextColor={colors.placeholderTextColor}
+				/>
+				<TextInput
+					style={styles.textInput}
+					secureTextEntry={true}
+					placeholder='Password'
+					value={password}
+					onChangeText={(text) => setPassword(text)}
+					placeholderTextColor={colors.placeholderTextColor}
+				/>
+			</View>
+			<View style={styles.buttonContainer}>
+				<Button buttonName='Login' onPress={registerHandler} />
+			</View>
+		</KeyboardAvoidingView>
+	);
 }
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-	paddingHorizontal: s(20),
-  },
-  headerContainer: {
-    marginBottom: vs(25),
-  },
-  headerContainerText: {
-    fontSize: mvs(40),
-    fontWeight: "bold",
-	color: colors.textColor,
-  },
-  captionText: {
-    fontSize: ms(20),
-    textAlign: "center",
-	color: colors.textColor,
-  },
-  formContainer: {
-    marginVertical: vs(25),
-  },
-  textInput: {
-    paddingVertical: vs(10),
-    paddingHorizontal: s(5),
-    borderBottomWidth: s(1),
-    width: s(300),
-    marginVertical: vs(5),
-    borderColor: colors.textColor,
-    color: colors.textColor,
-  },
-  buttonContainer: {
-	width: s(200),
-  },
+	mainContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		paddingHorizontal: s(20),
+	},
+	headerContainer: {
+		marginBottom: vs(25),
+	},
+	headerContainerText: {
+		fontSize: mvs(40),
+		fontWeight: "bold",
+		color: colors.textColor,
+	},
+	captionText: {
+		fontSize: ms(20),
+		textAlign: "center",
+		color: colors.textColor,
+	},
+	formContainer: {
+		marginVertical: vs(25),
+	},
+	textInput: {
+		paddingVertical: vs(10),
+		paddingHorizontal: s(5),
+		borderBottomWidth: s(1),
+		width: s(300),
+		marginVertical: vs(5),
+		borderColor: colors.textColor,
+		color: colors.textColor,
+	},
+	buttonContainer: {
+		width: s(200),
+	},
 });
